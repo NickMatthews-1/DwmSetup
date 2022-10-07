f1585ccd0f0f371736e474c30f7f52f3d69cfd29
Nick Matthews <matthews3102000@gmail.com>
2022-10-07 01:46:13 -0400

    Version 5 now.
    Added some big warning signals for my sanity and yours

--- DwmSetup.sh
+++ DwmSetup.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 ### This is the setup script for DWM
-
+echo -e "\e[0;31m PLEASE READ THE FOLLOWING \e[0m"
+echo -e "\e[0;31m PLEASE READ THE FOLLOWING \e[0m"
+echo -e "\e[0;31m PLEASE READ THE FOLLOWING \e[0m"
 echo "Please move the xinitrc file using the following"
 echo "cp /etc/X11/xinit/xinitrc .xinitrc"
-echo -e "\e[0;31m PLEASE READ THE FOLLOWING \e[0m"
-echo "If you do not do this then there will be issues"
+echo "If you do not do this then DWM"
+echo -e "\e[0;31m WILL NOT COMPILE \e[0m"
 
 # This downloads and installs the packages needed for DWM
 sudo pacman -S xorg-server xorg-xinit xorg-xinit xorg-xrandr xorg-xsetroot
