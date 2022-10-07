4c9bc9c803cd1b7f6292997320e71f93a6c07225
Nick Matthews <matthews3102000@gmail.com>
2022-10-07 01:56:49 -0400

    Stardate 76231.4

--- DwmSetup.sh
+++ DwmSetup.sh
@@ -6,7 +6,15 @@ echo -e "\e[0;31m PLEASE READ THE FOLLOWING \e[0m"
 echo -e "\e[0;31m PLEASE READ THE FOLLOWING \e[0m"
 echo "Please move the xinitrc file using the following"
 echo "cp /etc/X11/xinit/xinitrc .xinitrc"
-echo "If you do not do this then DWM " -e "\e[0;31m WILL NOT COMPILE \e[0m"
+echo -e "If you do not do this then DWM " "\e[0;31m WILL NOT COMPILE \e[0m"
+
+echo "Once you have moved the .xinitrc file"
+echo "go into it using an editor by doing"
+echo "vim .xinitrc"
+echo "Once in the .xinitrc file you need to do the following."
+echo -e "If this isn't done right then DWM" "\e[0;31m WILL NOT COMPILE \e[0m"
+echo "you need to remove the final 5 lines and replace it with"
+echo "exec dwm"
 
 # This downloads and installs the packages needed for DWM
 sudo pacman -S xorg-server xorg-xinit xorg-xinit xorg-xrandr xorg-xsetroot
