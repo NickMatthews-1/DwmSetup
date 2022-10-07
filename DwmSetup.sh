730c51b117469c5054db406cdc3b804574d572e7
Nick Matthews <matthews3102000@gmail.com>
2022-10-07 01:14:51 -0400

    DWM Setup Script
    This is the script that I have made.

---
+++ DwmSetup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+### This is the setup script for DWM
+
+# This downloads and installs the packages needed for DWM
+sudo pacman -S xorg-server xorg-xinit xorg-xinit xorg-xrandr xorg-xsetroot
+
+# This moves the xinitrc config to the correct place
+cp /etc/X11/xinit/xinitrc .xinitrc
+
+# This downloads the DWM git from suckless
+git clone https://git.suckless.org/dwm
