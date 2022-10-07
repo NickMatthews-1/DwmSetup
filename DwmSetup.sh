57bdf03692f912cecdd9536ae0bf931aa5bcb29b
Nick Matthews <matthews3102000@gmail.com>
2022-10-07 01:43:21 -0400

    Did I say 3rd version? I meant 4th.

--- DwmSetup.sh
+++ DwmSetup.sh
@@ -2,11 +2,13 @@
 
 ### This is the setup script for DWM
 
+echo "Please move the xinitrc file using the following"
+echo "cp /etc/X11/xinit/xinitrc .xinitrc"
+echo -e "\e[0;31m PLEASE READ THE FOLLOWING \e[0m"
+echo "If you do not do this then there will be issues"
+
 # This downloads and installs the packages needed for DWM
 sudo pacman -S xorg-server xorg-xinit xorg-xinit xorg-xrandr xorg-xsetroot
-
-# This moves the xinitrc config to the correct place
-cp /etc/X11/xinit/xinitrc .xinitrc
 
 # This moves up 1 directory
 cd ../
